b2b3ad3dde9a0d532bc5083c80f13faae093dfed
barteks79 <sade79dev@gmail.com>
2024-12-24 12:13:09 +0100

    feat: create header component, configure tailwind classes

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function Header() {
+   return (
+	  <header className="flex justify-center bg-gradient-primary bg-cover h-header font-instrument">
+		 <ul className="flex justify-between items-center w-header-wrapper text-white 2xl:font-medium">
+			<li className="hidden 2xl:inline text-header-small 2xl:text-header-big">
+			   <p>December 24, 2024</p>
+			</li>
+			<li className="flex items-center gap-5 h-full text-header-small 2xl:text-header-big">
+			   <p>Get 50% Off on your First pair of Shoes</p>
+			   <div className="hidden 2xl:block h-1/2 bg-white w-[1px]" />
+			   <p className="hidden 2xl:inline">Shop Now</p>
+			</li>
+			<li className="flex items-center gap-2 h-full text-header-small 2xl:text-header-big">
+			   <button className="px-2 py-1">English</button>
+			   <div className="block 2xl:hidden h-1/2 bg-white w-[1px]" />
+			   <Link className="px-1 py-1" href="/">Contact Us</Link>
+			</li>
+		 </ul>
+	  </header>
+   );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+   content: [
+	  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+	  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+	  './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+   ],
+   theme: {
+	  extend: {
+		 fontFamily: {
+			inter: ['"Inter"', '"Instrument Sans"', 'Arial'],
+			instrument: ['"Instrument Sans"', '"Inter"', 'Arial']
+		 },
+		 fontSize: {
+			'header-big': '14px',
+			'header-small': '11px'
+		 },
+		 height: {
+			header: '50px'
+		 },
+		 width: {
+			'header-wrapper': '85%',
+			'main-wrapper': '75%'
+		 },
+		 backgroundImage: {
+			'gradient-primary': 'url(\'/gradient.png\')'
+		 }
+
+	  }
+   },
+   plugins: []
 } satisfies Config;
